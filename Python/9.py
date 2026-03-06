6b518ed98780ee19e96537feba2730b31eed4590
Chernyshev Roman <triplets.rc@gmail.com>
2026-03-06 10:16:47 +0200

    Дописав

---
+++ Python/9.py
@@ -0,0 +1,14 @@
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+perimeter = (a + b + c)
+print("Perimeter of triangle: ", perimeter)
+
+# Написати програму, яка запитує в користувача координати точки дійсної площини, а потім виводить отриману точку
+# .Зверніть увагу на зовнішній вигляд друкованого результату.
+
+x = float(input("Enter the cordinate x: "))
+y = float(input("Enter the cordinate y: "))
+print(f"Points received: ({x}; {y})")
+
+
